25376d1f065a2e90cf169965af60a040e2f3cb84
esat <esatozbek@gmail.com>
2021-07-20 15:06:50 +0300

    Date removed

--- src/components/MessageList/views/MessageListItem/MessageListItem.js
+++ src/components/MessageList/views/MessageListItem/MessageListItem.js
@@ -5,7 +5,6 @@ function MessageListItem({ me, content, date }) {
     return (
         <div className={classNames(styles.container, { [styles.me]: me })}>
             <div className={styles.content}>{content}</div>
-            <div className={styles.date}>27.12.2021 12:12</div>
         </div>
     );
 }
